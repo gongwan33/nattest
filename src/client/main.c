378d50dd979a0a8056a818691061c996d9fd72a1
wanggong <gongwan33@126.com>
2013-06-20 13:33:32 +0800

    init server and client

---
+++ src/client/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("-------------------Nat Test Package Sending---------------------\n");
+
+	return 0;
+}
--- src/server/main.c
+++ src/server/main.c
@@ -1,6 +1,7 @@
-#include <stdio>
+#include <stdio.h>
 
 int main(){
+	printf("-------------------Nat Test Listening---------------------\n");
 
 	return 0;
 }
