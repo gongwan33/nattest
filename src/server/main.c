0cf74e72dc84c95117475d90fee7d718b1e5706b
wanggong <gongwan33@126.com>
2013-06-20 16:22:49 +0800

    server ok

--- src/server/main.c
+++ src/server/main.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define PORT1 61000
 
 int main(){
-	printf("-------------------Nat Test Listening---------------------\n");
+	int sin_len;
+	char recv[100];
+	int port = PORT1;
+
+	int sfd;
+	struct sockaddr_in sin;	
+	
+	printf("-------------------Nat Test Listening(UDP)---------------------\n");
+
+	while(1){
+		bzero(&sin, sizeof(sin));
+		sin.sin_family = AF_INET;
+		sin.sin_addr.s_addr = htonl(INADDR_ANY);
+		sin.sin_port = htons(port);
+		sin_len = sizeof(sin);
+				
+		sfd = socket(AF_INET, SOCK_DGRAM, 0);
+		if(!sfd) return -1;
+	
+		if(bind(sfd, (struct sockaddr *)&sin, sizeof(sin)) != 0){
+			printf("bind erro\n");
+			return -1;
+		}	
+		
+		printf("bind to port [%d]\n", port);
+
+		int i = 0;
+		for(i = 0; i < 5; i++){		
+			recvfrom(sfd, recv, sizeof(recv), 0, (struct sockaddr *)&sin, &sin_len);
+			printf("Recieve from %s [%d]:%s\n", inet_ntoa(sin.sin_addr), ntohs(sin.sin_port), recv);
+		}
+
+		if(port < (PORT1 + 2))port++;
+		else port = PORT1;
+
+		close(sfd);
+	}
+
+	close(sfd);
 
 	return 0;
 }
