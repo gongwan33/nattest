9ba876a256dca17154920b3f90fb023bf5bd561b
Administrator <administrator@ubuntu.ubuntu-domain>
2013-07-10 20:20:22 +0800

    nat类型设置beta

--- nat_set.sh
+++ nat_set.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sudo iptables -F -t nat
+sudo iptables -F
 echo "Please choose a nat mode:[1~4]"
 echo "[1]Full Cone NAT"
 echo "[2]Port Restricted Cone NAT"
@@ -8,20 +10,20 @@ read -p "Input your choice:" c1
 
 case $c1 in
 "1")
-  iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 202.96.209.111
-  iptables -t nat -A PREROUTING -i eth0 -j DNAT --to-destination 192.168.1.1
+  sudo iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 192.168.1.110 
+  sudo iptables -t nat -A PREROUTING -i eth0 -j DNAT --to-destination 192.10.1.1
   echo "Full Cone NAT setup completed!"
   ;;
 "2")
-  iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 202.96.209.111
+  sudo iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 192.168.1.110
   echo "Port Restricted Cone NAT setup completed!"
   ;;
 "3")
-  iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 202.96.209.111
-  iptables -t nat -A PREROUTING -i eth0 -j DNAT --to-destination 192.168.1.1 
-  iptables -A FORWARD -d 202.96.209.111 -j ACCEPT
-  iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-  iptables -I FORWARD 1 -s 85.214.128.86 -j ACCEPT
+  sudo iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 192.168.1.110
+  sudo iptables -t nat -A PREROUTING -i eth0 -j DNAT --to-destination 192.10.1.1 
+  sudo iptables -A FORWARD -d 192.168.1.110 -j ACCEPT
+  sudo iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+  sudo iptables -I FORWARD 1 -s 192.10.1.1 -j ACCEPT
   echo "Restricted Cone NAT setup completed!"
   ;;
 *)
