d00a607cfe032a015305c31862b92e7818977701
wanggong <seuic>
2013-09-04 11:17:45 +0800

    服务器端和客户端结构优化

--- p2p-src/client-master/main.c
+++ p2p-src/client-master/main.c
@@ -9,106 +9,142 @@
 #include <arpa/inet.h>
 #include <net/if.h>
 
-#define server_ip_1 "192.168.1.115"
+#define server_ip_1 "192.168.1.216"
 #define server_ip_2 "192.168.1.116"
-#define server_port 61000
-#define count 20 
 
-int main(){
-	int sockfd;
-	int n, i, j;
-	int port, sin_size;
-	char mac[6], ip[4], buff[1024];
-    	struct sockaddr_in servaddr1,servaddr2,local_addr;
-	struct ifreq ifr,*pifr;
-    	struct ifconf ifc;
-	char ip_info[50];
-	
-	memset (&local_addr, 0, sizeof local_addr);
-        local_addr.sin_family = AF_INET;
-        local_addr.sin_addr.s_addr = htonl (INADDR_ANY);
-        local_addr.sin_port = htons(1888);
-	
-	
-		
+#define USERNAME "wang"
+#define PASSWD "123456"
+
+#define ACT_NETCARD "eth1"
+#define server_port 61000
+#define local_port 6888
+
+static struct sockaddr_in servaddr1,servaddr2,local_addr;
+static struct ifreq ifr,*pifr;
+static struct ifconf ifc;
+static char ip_info[50];
+static int sockfd;
+static int port, sin_size;
+static char mac[6], ip[4], buff[1024];
+
+
+int local_net_init(){
+	memset(&local_addr, 0, sizeof local_addr);
+	local_addr.sin_family = AF_INET;
+	local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	local_addr.sin_port = htons(local_port);
+
 	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-   		printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
-    		return 0;
-   	}
-	
-	if (bind(sockfd, (struct sockaddr *) (&local_addr),sizeof(local_addr)) == -1){
-           	printf ("Bind error: %s\a\n", strerror (errno));
-           	return 0;
-        }
-	
+		printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
+		return -1;
+	}
+
+	if(bind(sockfd, (struct sockaddr *) (&local_addr),sizeof(local_addr)) == -1){
+		printf ("Bind error: %s\a\n", strerror (errno));
+		return -2;
+	}
+
+	return 0;
+}
+
+int get_local_ip_port(){
+	int i = 0;
 	sin_size = sizeof(local_addr);
 	getsockname(sockfd, (struct sockaddr*)&local_addr, &sin_size);
-        port=ntohs(local_addr.sin_port);
-	
+	port = ntohs(local_addr.sin_port);
+
 	memset(&ifr, 0, sizeof(ifr));
-    	ifc.ifc_len = sizeof(buff);
-    	ifc.ifc_buf = buff;
-    	if(ioctl(sockfd, SIOCGIFCONF, &ifc) < 0){
-        	printf("SIOCGIFCONF screwed up\n");
-        	return 0;
-    	}
+	ifc.ifc_len = sizeof(buff);
+	ifc.ifc_buf = buff;
 
-    	pifr = (struct ifreq *)(ifc.ifc_req);
-    	
+	if(ioctl(sockfd, SIOCGIFCONF, &ifc) < 0){
+		printf("SIOCGIFCONF screwed up\n");
+		return -1;
+	}
+
+	pifr = (struct ifreq *)(ifc.ifc_req);
+
 	for(i = ifc.ifc_len / sizeof(struct ifreq); --i >= 0; pifr++){
-        	strcpy(ifr.ifr_name, pifr->ifr_name);    //eth0 eth1 ...
-        	if(ioctl(sockfd, SIOCGIFADDR, &ifr) < 0){
-            		printf("ip screwed up\n");
-            		return 0;
-        	}
-        	memcpy(ip, ifr.ifr_addr.sa_data+2, 4);
-        	printf("%s\n",inet_ntoa(*(struct in_addr *)ip));
-    	}	
+		strcpy(ifr.ifr_name, pifr->ifr_name);    //eth0 eth1 ...
 
+		if(strcmp(ACT_NETCARD, ifr.ifr_name) != 0)
+			continue;
 
-
-
-	printf("%s\n", inet_ntoa(((struct sockaddr_in*)&(ifr.ifr_addr))->sin_addr));
-        printf("in child process port is: %d\n", port);
-	for(j = 0; j <3; j++){
-		memset(&servaddr1, 0, sizeof(servaddr1));
- 		servaddr1.sin_family = AF_INET;
-    		servaddr1.sin_port = htons(server_port+j);
-
-		memset(&servaddr2, 0, sizeof(servaddr2));
- 		servaddr2.sin_family = AF_INET;
-    		servaddr2.sin_port = htons(server_port+j);
-
-    		if( inet_pton(AF_INET, server_ip_1, &servaddr1.sin_addr) <= 0){
-    		printf("inet_pton error for %s\n",server_ip_1);
-    		exit(0);
-    		}
-
-		if( inet_pton(AF_INET, server_ip_2, &servaddr2.sin_addr) <= 0){
-    		printf("inet_pton error for %s\n",server_ip_2);
-    		exit(0);
-    		}
-
-		printf("-------------------Nat Test Package Sending---------------------\n");
-	
-		sprintf(ip_info,"#1 %s [%d]", inet_ntoa(*(struct in_addr *)ip), port);
-	
-		for(i = 0; i < count; i++){
-			sendto(sockfd, ip_info, sizeof(ip_info), 0, (struct sockaddr *)&servaddr1,sizeof(servaddr1)); 
+		if(ioctl(sockfd, SIOCGIFADDR, &ifr) < 0){
+			printf("ip screwed up\n");
+			return -2;
 		}
+		memcpy(ip, ifr.ifr_addr.sa_data+2, 4);
+		//printf("%s\n",inet_ntoa(*(struct in_addr *)ip));
+	}	
+
+	printf("local ip is %s\n",inet_ntoa(*(struct in_addr *)ip));
+	printf("local port is %d\n", port);
+
+	return 0;
+}
+
+int set_ip1_struct(char * ip1, int port){
+	memset(&servaddr1, 0, sizeof(servaddr1));
+	servaddr1.sin_family = AF_INET;
+	servaddr1.sin_port = htons(port);
+	
+	if( inet_pton(AF_INET, ip1, &servaddr1.sin_addr) <= 0){
+		printf("inet_pton error for %s\n",ip1);
+		return -1;
+	}
+
+	return 0;
+}
+
+int main(){
+	int  i;
+	int ret = 0;
+	
+	ret = local_net_init();
+	if(ret < 0){
+		printf("Local bind failed!!%d\n", ret);
+		return ret;
+	}
+
+	ret = get_local_ip_port();
+	if(ret < 0){
+		printf("Geting local ip and port failed!!%d\n", ret);
+		return ret;
+	}
+
+	ret = set_ip1_struct(server_ip_1, server_port);
+	if(ret < 0){
+		printf("Set ip1 failed!!%d\n", ret);
+		return ret;
+	}
+
+	printf("------------------- Connection and user name verifying ---------------------\n");
+	for(i = 0; i < 10; i++){
+
+		sprintf(ip_info,"%s %s", USERNAME, PASSWD);
+		printf("send %s\n", ip_info);
+
+		sendto(sockfd, ip_info, sizeof(ip_info), 0, (struct sockaddr *)&servaddr1,sizeof(servaddr1)); 
 
 		usleep(1000000);
-		
-		sprintf(ip_info,"#2 %s [%d]", inet_ntoa(*(struct in_addr *)ip), port);
+	}
 
-		for(i = 0; i < count; i++){
-			sendto(sockfd, ip_info, sizeof(ip_info), 0, (struct sockaddr *)&servaddr2,sizeof(servaddr2)); 
-		}
+	/*
+	   for(j = 0; j < 10; j++){
 
-	sleep(1);
-       	}
+	   sprintf(ip_info,"#1 %s [%d]", inet_ntoa(*(struct in_addr *)ip), port);
+	   printf("send %s\n", ip_info);
 
-  	close(sockfd);
-    	exit(0);	
+	   for(i = 0; i < count; i++){
+	   sendto(sockfd, ip_info, sizeof(ip_info), 0, (struct sockaddr *)&servaddr1,sizeof(servaddr1)); 
+	   }
+
+	   usleep(1000000);
+	   }
+	   */
+
+	close(sockfd);
+	exit(0);	
 	return 0;
 }
--- p2p-src/server/main.c
+++ p2p-src/server/main.c
@@ -1,114 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/ioctl.h>
 #include <arpa/inet.h>
-#include <net/if.h>
+#include <netdb.h>
 
-#define server_ip_1 "192.168.1.115"
-#define server_ip_2 "192.168.1.116"
-#define server_port 61000
-#define count 20 
+#define PORT1 61000
+#define ip1   "192.168.1.216"
+#define ip2   "192.168.1.116"
+
+static char pathname[50] = "./natinfo.log";
+static int sfd;
+static struct sockaddr_in sin;	
+static int sin_len;
+static char recv_str[100];
+static int port = PORT1;
+
+int local_net_init(){
+	bzero(&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	//sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	sin.sin_addr.s_addr = inet_addr(ip1);
+
+	sin.sin_port = htons(port);
+	sin_len = sizeof(sin);
+
+	sfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(!sfd) return -1;
+
+	if(bind(sfd, (struct sockaddr *)&sin, sizeof(sin)) != 0){
+		printf("bind erro\n");
+		return -2;
+	}	
+
+	printf("bind to port [%d]\n", port);
+
+	return 0;
+}
 
 int main(){
-	int sockfd;
-	int n, i, j;
-	int port, sin_size;
-	char mac[6], ip[4], buff[1024];
-    	struct sockaddr_in servaddr1,servaddr2,local_addr;
-	struct ifreq ifr,*pifr;
-    	struct ifconf ifc;
-	char ip_info[50];
-	
-	memset (&local_addr, 0, sizeof local_addr);
-        local_addr.sin_family = AF_INET;
-        local_addr.sin_addr.s_addr = htonl (INADDR_ANY);
-        local_addr.sin_port = htons(1888);
-	
-	
-		
-	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-   		printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
-    		return 0;
-   	}
-	
-	if (bind(sockfd, (struct sockaddr *) (&local_addr),sizeof(local_addr)) == -1){
-           	printf ("Bind error: %s\a\n", strerror (errno));
-           	return 0;
-        }
-	
-	sin_size = sizeof(local_addr);
-	getsockname(sockfd, (struct sockaddr*)&local_addr, &sin_size);
-        port=ntohs(local_addr.sin_port);
-	
-	memset(&ifr, 0, sizeof(ifr));
-    	ifc.ifc_len = sizeof(buff);
-    	ifc.ifc_buf = buff;
-    	if(ioctl(sockfd, SIOCGIFCONF, &ifc) < 0){
-        	printf("SIOCGIFCONF screwed up\n");
-        	return 0;
-    	}
+	int ret = 0;
+	ret = local_net_init();
+	if(ret < 0){
+		printf("local net init failed!!\n");
+		return ret;
+	}
 
-    	pifr = (struct ifreq *)(ifc.ifc_req);
-    	
-	for(i = ifc.ifc_len / sizeof(struct ifreq); --i >= 0; pifr++){
-        	strcpy(ifr.ifr_name, pifr->ifr_name);    //eth0 eth1 ...
-        	if(ioctl(sockfd, SIOCGIFADDR, &ifr) < 0){
-            		printf("ip screwed up\n");
-            		return 0;
-        	}
-        	memcpy(ip, ifr.ifr_addr.sa_data+2, 4);
-        	printf("%s\n",inet_ntoa(*(struct in_addr *)ip));
-    	}	
+	printf("-------------------Wait for Login---------------------\n");
+	int i = 0;
+	for(i = 0; i < 10; i++){		
+		recvfrom(sfd, recv_str, sizeof(recv_str), 0, (struct sockaddr *)&sin, &sin_len);
+		printf("Recieve from %s [%d]:%s\n", inet_ntoa(sin.sin_addr), ntohs(sin.sin_port), recv_str);
+	}
+	close(sfd);
 
-
-
-
-	printf("%s\n", inet_ntoa(((struct sockaddr_in*)&(ifr.ifr_addr))->sin_addr));
-        printf("in child process port is: %d\n", port);
-	for(j = 0; j <3; j++){
-		memset(&servaddr1, 0, sizeof(servaddr1));
- 		servaddr1.sin_family = AF_INET;
-    		servaddr1.sin_port = htons(server_port+j);
-
-		memset(&servaddr2, 0, sizeof(servaddr2));
- 		servaddr2.sin_family = AF_INET;
-    		servaddr2.sin_port = htons(server_port+j);
-
-    		if( inet_pton(AF_INET, server_ip_1, &servaddr1.sin_addr) <= 0){
-    		printf("inet_pton error for %s\n",server_ip_1);
-    		exit(0);
-    		}
-
-		if( inet_pton(AF_INET, server_ip_2, &servaddr2.sin_addr) <= 0){
-    		printf("inet_pton error for %s\n",server_ip_2);
-    		exit(0);
-    		}
-
-		printf("-------------------Nat Test Package Sending---------------------\n");
-	
-		sprintf(ip_info,"#1 %s [%d]", inet_ntoa(*(struct in_addr *)ip), port);
-	
-		for(i = 0; i < count; i++){
-			sendto(sockfd, ip_info, sizeof(ip_info), 0, (struct sockaddr *)&servaddr1,sizeof(servaddr1)); 
-		}
-
-		usleep(1000000);
-		
-		sprintf(ip_info,"#2 %s [%d]", inet_ntoa(*(struct in_addr *)ip), port);
-
-		for(i = 0; i < count; i++){
-			sendto(sockfd, ip_info, sizeof(ip_info), 0, (struct sockaddr *)&servaddr2,sizeof(servaddr2)); 
-		}
-
-	sleep(1);
-       	}
-
-  	close(sockfd);
-    	exit(0);	
 	return 0;
 }
